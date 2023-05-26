3451dc492f78ac0106055c65fe95c50037088d6f
LMitrovic6120RN <Mitrovic21>
2023-05-26 19:02:23 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <nav>
+
+
+    <Navigation_Bar/>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
   </div>
 </template>
+
+<script>
+  import Navigation_Bar from '../src/components/NavigationBar.vue';
+  export default{
+    components: {Navigation_Bar}
+  }
+</script>
 
 <style>
 #app {
